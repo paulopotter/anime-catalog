763161deddbf6d2de295447ded33264323f2b053
Paulo Oliveira <paulo.oliveira@corp.globo.com>
2017-03-10 19:15:56 -0300

    Read all descriptions on all folders

--- catalogar.py
+++ catalogar.py
@@ -1,2 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+
+import os
+import simplejson as json
+
+
+def get_folder_to_catalog():
+    parent_folder = os.path.dirname(os.getcwd())
+
+    return parent_folder + '/Animes'
+
+
+def get_description(folder, file_name):
+    file = folder + '/' + file_name
+    with open(file) as data:
+        description = json.load(data)
+
+    return description
+
+
+get_all_folders_names = os.listdir(get_folder_to_catalog())
+current_folder = get_folder_to_catalog()
+catalog_descriptions = {}
+
+for folder_name in get_all_folders_names:
+    folder = current_folder + '/' + folder_name
+    catalog_descriptions[folder_name] = get_description(folder, 'descricao.json')
+
+
+print catalog_descriptions
