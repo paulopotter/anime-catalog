8006853ea1bb9d1b26dd152569746815f8cc8d84
Paulo Oliveira <paulofrauches@gmail.com>
2017-03-13 19:15:31 -0300

    fix list only folders

--- catalogar.py
+++ catalogar.py
@@ -28,13 +28,27 @@ def break_folders_by_letter(folders):
     return letters
 
 
-get_all_folders_names = os.listdir(get_folder_to_catalog())
+def is_folder(folder_path, folder_name):
+     return os.path.isdir(folder_path + '/' + folder_name)
+
+
+def get_all_folders(folder_content, folder_path):
+    all_folders = []
+    for content_name in folder_content:
+        if is_folder(folder_path, content_name):
+            all_folders.append(content_name)
+
+    return all_folders
+
+
+
 current_folder = get_folder_to_catalog()
+get_all_folders_names = get_all_folders(os.listdir(get_folder_to_catalog()), current_folder)
 catalog_descriptions = {}
 
 for folder_name in get_all_folders_names:
     folder = current_folder + '/' + folder_name
-    catalog_descriptions[folder_name] = get_description(folder, 'descricao.json')
+    catalog_descriptions[folder_name] = get_description(folder, 'description.json')
 
 break_by_letters = break_folders_by_letter(get_all_folders_names)
 
