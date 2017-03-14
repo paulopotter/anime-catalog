143bb1cf2a2380d647f9442dfccfc0bea895e1f8
Paulo Oliveira <paulofrauches@gmail.com>
2017-03-13 21:30:59 -0300

    Print all descriptions

--- catalogar.py
+++ catalogar.py
@@ -9,19 +9,24 @@ import simplejson as json
 class Cataloguer():
 
 
-    def __init__(self):
-        current_folder = self.get_folder_to_catalog('Animes')
-        get_all_folders_names = self.get_all_folders(os.listdir(current_folder), current_folder)
+    def __init__(self, folder_to_catalog, description_file):
+        self.current_folder = self.get_folder_to_catalog(folder_to_catalog)
+        get_all_folders_names = self.get_all_folders(os.listdir(self.current_folder), self.current_folder)
         catalog_descriptions = {}
 
         for folder_name in get_all_folders_names:
-            folder = current_folder + '/' + folder_name
-            catalog_descriptions[folder_name] = self.get_description(folder, 'description.json')
+            folder = self.current_folder + '/' + folder_name
+            catalog_descriptions[folder_name] = self.get_description(folder, description_file)
 
-        break_by_letters = self.break_folders_by_letter(get_all_folders_names)
+        folders_by_letters = self.break_alphabetically(get_all_folders_names)
 
-        print break_by_letters
+        for letter in folders_by_letters.keys():
+            all_content_in_this_letter = folders_by_letters[letter]
+            size_of_content_in_this_letter = len(all_content_in_this_letter)
 
+            for i in range(size_of_content_in_this_letter):
+                print catalog_descriptions[all_content_in_this_letter[i]]
+                print '\n'
 
 
     def get_folder_to_catalog(self, folder):
@@ -38,7 +43,7 @@ class Cataloguer():
         return description
 
 
-    def break_folders_by_letter(self, folders):
+    def break_alphabetically(self, folders):
         letters = {}
 
         for folder in folders:
@@ -61,6 +66,5 @@ class Cataloguer():
 
 
 
-
 if __name__ == "__main__":
-    Cataloguer()
+    Cataloguer('Animes', 'description.json')
