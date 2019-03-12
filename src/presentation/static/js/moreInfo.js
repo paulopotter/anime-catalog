ecb02184272cbdcc7e0e383633906e421f7a19a2
Paulo Oliveira <paulofrauches@gmail.com>
2019-03-11 23:23:08 -0300

    fill when there are otherSeasons

--- src/presentation/static/js/moreInfo.js
+++ src/presentation/static/js/moreInfo.js
@@ -1,4 +1,6 @@
-let fillHeader = (item) => {
+let fillHeader = (elementId) => {
+    let el = document.querySelector(`#${elementId}`);
+    let item = data[el.getAttribute('data-index')][el.getAttribute('data-position')]
     let header = document.getElementById('header');
     let itemImg = item.path + '/thumb.png';
 
@@ -13,14 +15,32 @@ let fillHeader = (item) => {
     header.querySelector('.header__info--totalEpisodes').innerText = item.totalEpisodes || '';
     // header.querySelector('.header__info--episodesDownloaded').innerText = item.episodesDownloaded || '';
     header.querySelector('.header__info--description').innerText = item.description;
-    header.querySelector('.header__info--othersSeasons').innerText = item.othersSeasons || '';
+    header.querySelector('.header__info--othersSeasons').innerHTML = '';
+    item.othersSeasons.forEach(element => {
+        header.querySelector('.header__info--othersSeasons').innerHTML += `<span>${element}</span>`
+    });
     header.querySelector('.header__info--obs').innerText = item.obs || '';
+}
+const moveItem = (item) => {
+    item.parentElement.scroll({
+        left: item.offsetLeft - 70,
+        top: 0,
+        behavior: 'smooth'
+    });
+    history.replaceState('', '', '#' + item.innerText.slugify())
 }
 
 for (let item of document.querySelectorAll('.catalog__item')) {
     item.addEventListener('click', (ev) => {
         let selectedItem = ev.target.parentElement.parentElement;
-        let item = data[selectedItem.getAttribute('data-index')][selectedItem.getAttribute('data-position')]
-        fillHeader(item)
+        fillHeader(selectedItem.id)
+        moveItem(selectedItem);
     }, false)
 }
+
+document.querySelector('.header__info--othersSeasons')
+    .addEventListener('click', (ev) => {
+        let selectedItemName = ev.target.innerText.slugify()
+        moveItem(document.getElementById(selectedItemName));
+        fillHeader(selectedItemName)
+    }, false)
