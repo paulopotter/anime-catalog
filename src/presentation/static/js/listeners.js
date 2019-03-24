1192a0950d9be2d636ac53290b2d24e9e2195481
Paulo Oliveira <paulofrauches@gmail.com>
2019-03-24 10:50:41 -0300

    Refactor listeners

--- src/presentation/static/js/listeners.js
+++ src/presentation/static/js/listeners.js
@@ -1,16 +1,22 @@
-document.querySelectorAll('.catalog__item').forEach(item => {
-    item.addEventListener('click', (ev) => {
-        if (document.getElementById('header').classList.contains('header__more-info--open')) {
-            document.getElementById('header').classList.remove('header__more-info--open')
-        } else {
-            let selectedItem = ev.target.parentElement.parentElement;
-            fillWithSelectedItem(selectedItem);
-            document.getElementById('header').classList.add('header__more-info--open')
-        }
-    }, false)
-})
+const HEADER = document.getElementById('header');
+const SEARCH = document.getElementById('search');
+const SEARCH_INPUT = SEARCH.querySelector('.search__input');
 
-document.querySelector('.header__info--othersSeasons')
+
+document.querySelectorAll('.catalog__item')
+    .forEach(item => {
+        item.addEventListener('click', (ev) => {
+            const headerClass = HEADER.classList;
+            if (headerClass.contains('header__more-info--open')) {
+                headerClass.remove('header__more-info--open');
+            } else {
+                fillWithSelectedItem(ev.target.parentElement.parentElement);
+                headerClass.add('header__more-info--open');
+            }
+        }, false)
+    })
+
+HEADER.querySelector('.header__info--othersSeasons')
     .addEventListener('click', (ev) => {
         if (ev.target.innerHTML.indexOf('<li>') == -1) {
             let selectedItemName = ev.target.innerText.slugify()
@@ -18,134 +24,103 @@ document.querySelector('.header__info--othersSeasons')
         }
     }, false)
 
-document.getElementById('header')
-    .addEventListener('click', (evt) => {
-        if(evt.target.nodeName != "LI"){
-            document.getElementById('header').classList.add('header__more-info--open')
-        }
-    }, false)
-
-// document.getElementById('catalog')
-//     .addEventListener('click', () => {
-//         document.getElementById('header').classList.remove('header__more-info--open')
-//     }, false)
-
-document.onkeydown = (evt) => {
+document.addEventListener('keydown', (evt) => {
     evt = evt || window.event;
     const animeItems = document.querySelectorAll('.catalog__item:not(.hidden)');
-    let showInfo = document.querySelector('.catalog__item--selected:not(.hidden)');
+    const showInfo = document.querySelector('.catalog__item--selected:not(.hidden)');
+    const headerClasses = HEADER.classList;
+    const isHeaderOpen = headerClasses.contains('header__more-info--open');
+    const isSearchInputActive = document.activeElement.classList.contains('search__input');
+    const navigate = isHeaderOpen && !isSearchInputActive ? false : isHeaderOpen ? false : !isSearchInputActive;
 
-        switch (evt.keyCode) {
-            case 13: // 13 - Enter
-                document.getElementById('header').classList.add('header__more-info--open')
-            break;
+    switch (evt.keyCode) {
+        case 13: // 13 - Enter
+            HEADER.classList.add('header__more-info--open');
+        break;
 
-            case 27: // 27 - ESC
-                document.getElementById('header').classList.remove('header__more-info--open')
-                document.getElementById('search').classList.remove('open')
-                document.querySelector('input').blur()
-            break;
+        case 27: // 27 - ESC
+            HEADER.classList.remove('header__more-info--open')
+            SEARCH.classList.remove('open')
+            SEARCH_INPUT.blur()
+        break;
 
-            case 37: // 37 - left
-                let headerL = document.getElementById('header').classList.contains('header__more-info--open')
-                let serL = !document.activeElement.classList.contains('search__input')
-                let navL = headerL && serL ? false : headerL ? false : serL;
+        case 37: // 37 - left
+            navigate ? fillWithSelectedItem(getPreviousSibling(showInfo, ':not(.hidden)')) : null
+        break;
 
-                navL ? fillWithSelectedItem(getPreviousSibling(showInfo, ':not(.hidden)')) : null
-            break;
+        case 39: // 39 - right
+            navigate ? fillWithSelectedItem(getNextSibling(showInfo, ':not(.hidden)')) : null
+        break;
 
-            case 39: // 39 - right
-                let headerR = document.getElementById('header').classList.contains('header__more-info--open')
-                let serR = !document.activeElement.classList.contains('search__input')
-                let navR = headerR && serR ? false : headerR ? false : serR;
-                navR ? fillWithSelectedItem(getNextSibling(showInfo, ':not(.hidden)')) : null
-            break;
-
-            case 38: // up
-                evt.preventDefault()
-                 let headerU = document.getElementById('header').classList.contains('header__more-info--open')
-                 let serU = !document.activeElement.classList.contains('search__input')
-                 let navU = headerU && serU ? false : headerU ? false : serU;
-                if (!navU) {
-                    evt.preventDefault()
-                } else {
-
+        case 38: // up
+            evt.preventDefault()
+            if (navigate) {
                 let goal = (showInfo.offsetTop - showInfo.offsetHeight - 10)
-                if (goal <=0 && isElementVisible(document.querySelector('input'))) {
-                        document.querySelector('input').focus()
-                }
-                animeItems.forEach(item => {
 
-                    if (goal > 0 &&
+                if (goal <=0 && isElementVisible(SEARCH)){
+                    !isSearchInputActive ? document.querySelector('input').focus() : null
+                } else {
+                    animeItems.forEach(item => {
+                        if (goal > 0 &&
+                            (closest(animeItems, goal) == item.offsetTop) &&
+                            (showInfo.offsetLeft == item.offsetLeft)) {
+
+                            fillWithSelectedItem(item);
+                        }
+                    })
+                }
+            } else {
+                evt.preventDefault()
+            }
+        break;
+
+        case 40: // Down
+            evt.preventDefault()
+            if(navigate){
+                let goal = (showInfo.offsetTop + showInfo.offsetHeight + 10)
+                animeItems.forEach(item => {
+                    if (
                         (closest(animeItems, goal) == item.offsetTop) &&
                         (showInfo.offsetLeft == item.offsetLeft)
                     ) {
                         fillWithSelectedItem(item);
                     }
                 })
-            }
-            break;
-
-            case 40: // Down
-                evt.preventDefault()
-
-                let headerD = document.getElementById('header').classList.contains('header__more-info--open')
-                let serD = !document.activeElement.classList.contains('search__input')
-                let navD = headerD && serD ? false : headerD ? false : serD;
-
-
-                if (!navD) {
-                    document.querySelector('input').blur()
-                    if(showInfo){
-                        fillWithSelectedItem(showInfo);
-                    } else {
-
-                        fillWithSelectedItem(animeItems[0]);
-                    }
+            } else {
+                SEARCH_INPUT.blur()
+                if(showInfo){
+                    fillWithSelectedItem(showInfo);
                 } else {
-                    let goal = (showInfo.offsetTop + showInfo.offsetHeight + 10)
-                    animeItems.forEach(item => {
-                        if (
-                            (closest(animeItems, goal) == item.offsetTop) &&
-                            (showInfo.offsetLeft == item.offsetLeft)
-                        ) {
-                            fillWithSelectedItem(item);
-                        }
-                    })
+                    fillWithSelectedItem(animeItems[0]);
                 }
-            break;
 
-            case 70: // F
-                let search = document.getElementById('search')
-                if (!search.classList.contains('open')) {
-                    search.classList.add('open');
-                    search.querySelector('input').focus(evt.preventDefault())
-                }
-            break
-        }
-};
+            }
+        break;
 
-document.querySelector('.search__button')
+        case 70: // F
+            if (!SEARCH.classList.contains('open')) {
+                SEARCH.classList.add('open');
+                SEARCH_INPUT.focus(evt.preventDefault())
+            }
+        break
+    }
+});
+
+SEARCH.querySelector('.search__button')
     .addEventListener('click', () => {
-        let searchClass = document.getElementById('search').classList
-        if(searchClass.contains('open') ){
-            searchClass.remove('open')
-        }else {
-            searchClass.add('open')
-        }
+        let searchClass = SEARCH.classList
+        (searchClass.contains('open') ) ? searchClass.remove('open') : searchClass.add('open')
 })
 
-document.querySelector('.search__input')
+SEARCH_INPUT
     .addEventListener('keydown', evt => {
         let value = evt.target.value
         const animeItems = document.querySelectorAll('.catalog__item');
         if (value.length >= 2){
-            if (event.keyCode >= 48 && event.keyCode <= 90 || event.keyCode == 8) {
-                let found = document.querySelectorAll(`[id*='${value.slugify()}']`)
-                animeItems.forEach(item => {
-                    item.classList.add('hidden')
-                })
-                found.forEach(item => {
+
+            if (evt.keyCode >= 48 && evt.keyCode <= 90 || evt.keyCode == 8) {
+                animeItems.forEach(item => { item.classList.add('hidden')})
+                document.querySelectorAll(`[id*='${value.slugify()}']`).forEach(item => {
                     item.classList.remove('hidden')
                 })
 
@@ -153,10 +128,7 @@ document.querySelector('.search__input')
 
         } else {
             if ((!event.keyCode >= 37 && !event.keyCode <= 39 ) || event.keyCode == 8) {
-
-                animeItems.forEach(item => {
-                    item.classList.remove('hidden');
-                })
+                animeItems.forEach(item => { item.classList.remove('hidden'); })
                 fillWithSelectedItem(animeItems[0])
             }
         }
